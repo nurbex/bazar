b788f9d59eed48a01be88c7bfb3f8763e49f32b1
Nurbek <nurbekaj@gmail.com>
2021-06-05 20:17:14 -0500

    Changed how the look of admin dashboard

--- src/main/java/com/eshop/bazar/controllers/ProductController.java
+++ src/main/java/com/eshop/bazar/controllers/ProductController.java
@@ -61,7 +61,6 @@ public class ProductController {
 
     @PostMapping("/edit")
     public String saveProduct(Product product){
-
         productService.createOrUpdateProduct(product);
         return "redirect:/admin/products";
     }
